78952d291a12d15e99f14ce1819e86a7f25e594e
Bhavyai Gupta <zbhavyai@gmail.com>
2021-06-12 20:31:40 +0530

    made flow of menu more intuitive

--- menu.py
+++ menu.py
@@ -4,7 +4,20 @@
 
 import ansi_colors as color
 from custom_errors import ValueOutOfRange
+import time
 import os
+
+
+def clear_console():
+    """Function to clear the console
+
+        Parameters:
+            none
+
+        Returns:
+            None
+    """
+    print("\033\143")
 
 
 def splash_message():
@@ -21,23 +34,17 @@ def splash_message():
     """
     os.system("")
 
+    clear_console()
+
     print(color.magenta)
     print("---------------------------------------------------")
     print(" Welcome to UN Population and Education Statistics")
     print("---------------------------------------------------")
     print(color.reset, end="")
 
-
-def clear_console():
-    """Function to clear the console
-
-        Parameters:
-            none
-
-        Returns:
-            None
-    """
-    print("\033\143")
+    # show the splash screen for 2 seconds and then clear again
+    time.sleep(2)
+    clear_console()
 
 
 def program_menu():
@@ -51,48 +58,62 @@ def program_menu():
         Returns:
             None
     """
-    print("\n" + color.yellow + "Program menu" + color.reset + "\n")
-    print("[1] Print stats for relation between Life Expectancy and Urbanization")
-    print("[2] Print stats for relation between Tertiary Education in all countries")
-    print("[3] Print stats for relation between Fertility and Literacy")
-    print("[4] Print stats for relation between Urbanization and Fertility")
-    print("[0] Exit")
-
+    # loop to keep printing the menu until Exit
     while(True):
-        try:
-            choice = int(input("\nPlease enter the menu option number (1-4 or 0): "))
+        # clear the console everytime menu is printed
+        clear_console()
 
-            if choice == 0:
-                return
+        print("\n" + color.yellow + "Program menu" + color.reset + "\n")
+        print("[1] Print stats for relation between Life Expectancy and Urbanization")
+        print("[2] Print stats for relation between Tertiary Education in all countries")
+        print("[3] Print stats for relation between Fertility and Literacy")
+        print("[4] Print stats for relation between Urbanization and Fertility")
+        print("[0] Exit")
 
-            elif choice == 1:
-                # call function for stats for Life Expectancy and Urbanization
-                pass
+        # loop to get valid choice from the user
+        while(True):
+            try:
+                choice = int(input("\nPlease enter the menu option number (1-4 or 0): "))
 
-            elif choice == 2:
-                # # call function for stats for Tertiary Education
-                pass
+                if choice < 0 or choice > 4:
+                    raise ValueOutOfRange("This option is not supported. Please choose a valid menu option")
 
-            elif choice == 3:
-                # call function for stats for Fertility and Literacy
-                pass
+                else:
+                    break
 
-            elif choice == 4:
-                # call function for Urbanization and Fertility
-                pass
+            except ValueError as e:
+                print("\n" + color.red + "Please enter a valid menu option" + color.reset)
 
-            else: # choice < 0 or choice > 4:
-                raise ValueOutOfRange("This option is not supported. Please choose a valid menu option")
+            except ValueOutOfRange as e:
+                print("\n" + color.red + str(e) + color.reset)
 
-        except ValueError as e:
-            print("\n" + color.red + "Please enter a valid menu option" + color.reset)
+            except KeyboardInterrupt as e:
+                print("\nYou pressed Ctrl+C")
+                print("\n\nBye!\n")
 
-        except ValueOutOfRange as e:
-            print("\n" + color.red + str(e) + color.reset)
 
+        # after getting a valid option from the user, perform the requested option
+        if choice == 1:
+            # call function for stats for Life Expectancy and Urbanization
+            pass
+
+        elif choice == 2:
+            # # call function for stats for Tertiary Education
+            pass
+
+        elif choice == 3:
+            # call function for stats for Fertility and Literacy
+            pass
+
+        elif choice == 4:
+            # call function for Urbanization and Fertility
+            pass
+
+        else:   # choice == 0:
+            print("\n\nBye!\n")
+            return
 
 
 if __name__ == '__main__':
-    clear_console()
     splash_message()
     program_menu()
